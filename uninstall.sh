f5f926c2ca661ddc3eea0c7736512634ae4d56ff
Orachimaru <ardaktleules11@gmail.com>
2025-06-21 18:33:59 +0500

    add docker cmd to clean build cashe

--- uninstall.sh
+++ uninstall.sh
@@ -3,7 +3,7 @@ docker stop dockerize 2>/dev/null
 docker rm dockerize 2>/dev/null
 
 docker rmi ascii-art-web-docker 2>/dev/null
-
+docker builder prune 2>/dev/null
 if [ -f ascii-art-web-docker.tar ]; then
 rm ascii-art-web-docker.tar
 fi
